bf52a096edeafb54a4e6f6463e1c74af3354a96a
Harish-55 <harishcs2002@gmail.com>
2024-02-08 11:28:36 +0530

    Remove Plugin - tinycomments

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -36,7 +36,7 @@
             selector: 'textarea.tinymce-editor',
             height: 300,
             menubar: false,
-            plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+            plugins: 'ai mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
             toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
             content_css: "//www.tiny.cloud/css/codepen.min.css",
             ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
