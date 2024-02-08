c1e83b800f1037a71f521fb71645ecfae505bff8
Harish-55 <harishcs2002@gmail.com>
2024-02-08 11:13:18 +0530

    Tiny Text editor correction

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -30,6 +30,19 @@
             </div>
         </div>
     </section>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea.tinymce-editor',
+            height: 300,
+            menubar: false,
+            plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            content_css: "//www.tiny.cloud/css/codepen.min.css",
+            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+        });
+    </script>
+
 </body>
 
 <div id="script">
