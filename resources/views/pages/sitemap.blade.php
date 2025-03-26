5e410b998cc77252fb69257ce75538b23f1f2183
Arockia Rejo <95269579+ArockiaRejoJ@users.noreply.github.com>
2025-03-26 12:43:21 +0530

    site-map-dhanges

--- resources/views/pages/sitemap.blade.php
+++ resources/views/pages/sitemap.blade.php
@@ -1,8 +1,5 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 
 
     <url>
@@ -270,6 +267,7 @@
         <lastmod>2025-03-26T06:03:29+00:00</lastmod>
         <priority>0.51</priority>
     </url>
+
 
     @foreach ($data as $sitemap)
         <url>
